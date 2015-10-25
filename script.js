8c6a0f24c5bd76495e64008d44aa62101a2d293f
Grzegorz Dobiecki <g.dobiecki@gmail.com>
2015-10-25 18:40:29 +0100

    generate access token - WIP

--- script.js
+++ script.js
@@ -1,52 +1,36 @@
 $('document').ready(function() {
 
-    function getDataFromJson() {
+    function generateUrl() {
         $.getJSON("config/data.json", function (json) {
             var clientId = json.data.clientId,
                 clientSecret = json.data.clientSecret,
                 websiteUrl = json.data.websiteUrl,
-                redirectUrl = json.data.redirectUrl;
+                redirectUrl = json.data.redirectUrl,
+                url = 'https://api.instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirectUrl + '&response_type=code';
 
-            dane = {
-                clientId: clientId,
-                clientSecret: clientSecret,
-                websiteUrl: websiteUrl,
-                redirectUrl: redirectUrl
-            };
+            document.getElementById("authorize").setAttribute("href", url);
 
+            var link = $("a[id='authorize']");
+
+            link.on('click', function() {
+                $.ajax({
+                    method: "POST",
+                    dataType: "jsonp",
+                    data: {
+                        client_id: clientId,
+                        client_secret: clientSecret,
+                        grant_type: 'authorization_code',
+                        redirect_url: redirectUrl,
+                        code: getCode()
+                    },
+                    url: "https://api.instagram.com/oauth/access_token",
+                    success: function (data) {
+                        console.log(data);
+                    }
+                })
+            })
         })
     }
-
-
-    function generateUrl() {
-
-        console.log(getDataFromJson(callback));
-        var data = getDataFromJson(),
-            clientId = data.clientId,
-            clientSecret = data.clientSecret,
-            websiteUrl = data.websiteUrl,
-            redirectUrl = data.redirectUrl,
-            url = 'https://api.instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirectUrl + '&response_type=code';
-
-        document.getElementById("authorize").setAttribute("href", url);
-
-        $.ajax({
-            method: "POST",
-            dataType: "jsonp",
-            data: {
-                client_id: clientId,
-                client_secret: clientSecret,
-                grant_type: 'authorization_code',
-                redirect_url: redirectUrl,
-                code: getCode()
-            },
-            url: "https://api.instagram.com/oauth/access_token",
-            success: function (data) {
-                console.log(data);
-            }
-        })
-    }
-
 
     function getCode() {
         var code = 'code',
